2e5fe6e1dd27042428ed6cce0102068f86a2fbe5
Leon 3D <funarchitect2022@gmail.com>
2014-03-24 14:13:28 -0700

    synchronize name changing, required for destruction/init sync

---
+++ NetworkChangeName.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+public class NetworkChangeName extends MonoBehaviour
+{
+
+	public function setName(name : String)
+	{
+		this.gameObject.name = name;
+		networkView.RPC("networkChangeName", RPCMode.Others, this.gameObject.name);
+	}
+
+	@RPC
+	public function networkChangeName(nameOther : String)
+	{
+		this.gameObject.name = nameOther;
+	}
+}
